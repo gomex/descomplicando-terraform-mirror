7f755ce2b014f3d420e3d589cb4534381783a6ee
Gomex <linux.rafa@gmail.com>
2020-08-29 10:03:34 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+module "ec2" {
+  source                  = "git@github.com:gomex/descomplicando-terraform-modulo-mirror.git"
+  app_name                = "ao-vivo-turma3"
+  instance_type           = "t3.micro"
+}
+
+output "ip_address_ec2" {
+  value = module.ec2.ip_address
+}
